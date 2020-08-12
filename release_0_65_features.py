a3a1b7a8be07b7055646775b2468a5f7ea3748be
karrie <karrie@streamlit.io>
2020-08-11 23:55:02 -0400

    Comment out query params for s4t deployment

--- release_0_65_features.py
+++ release_0_65_features.py
@@ -23,23 +23,23 @@ st.beta_set_page_config(
 )
 '''.format(PAGE_CONFIG["icon"], PAGE_CONFIG["layout"], PAGE_CONFIG["sidebar"]),
     },
-    "Query Params [experimental]": {
-        "text":"We have added to our experimental namespace the ability to get and set query parameters. With these query params, you can bookmark or share your app in various states. Thanks [@zhaoooyue](https://github.com/zhaoooyue) for the contribution!",
-        "code":'''
-radio_list = ['Eat', 'Sleep', 'Both']
-query_params = st.experimental_get_query_params()
-# Query parameters are returned as a list to support multiselect.
-# Get the first item in the list if the query parameter exists.
-default = int(query_params["activity"][0]) if "activity" in query_params else 0
-activity = st.radio(
-    "What are you doing at home during quarantine?",
-    radio_list,
-    index = default
-)
-if activity:
-    st.experimental_set_query_params(activity=radio_list.index(activity))
-        ''',
-    },
+#     "Query Params [experimental]": {
+#         "text":"We have added to our experimental namespace the ability to get and set query parameters. With these query params, you can bookmark or share your app in various states. Thanks [@zhaoooyue](https://github.com/zhaoooyue) for the contribution!",
+#         "code":'''
+# radio_list = ['Eat', 'Sleep', 'Both']
+# query_params = st.experimental_get_query_params()
+# # Query parameters are returned as a list to support multiselect.
+# # Get the first item in the list if the query parameter exists.
+# default = int(query_params["activity"][0]) if "activity" in query_params else 0
+# activity = st.radio(
+#     "What are you doing at home during quarantine?",
+#     radio_list,
+#     index = default
+# )
+# if activity:
+#     st.experimental_set_query_params(activity=radio_list.index(activity))
+#         ''',
+#     },
     "Improved Dataframe Support": {
         "text":"""
 With 0.65, there is additional support for pandas dataframes. These components can now take in a panda dataframe as a list of options:
