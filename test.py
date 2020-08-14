5738b57b38e5698e9cc6bcaaa41a5686e30365ca
karrie <karrie@streamlit.io>
2020-08-14 15:44:42 -0400

    i have no idea what im doing anymore

--- test.py
+++ test.py
@@ -9,26 +9,24 @@ st.beta_set_page_config(
     initial_sidebar_state="collapsed"
 )
 
-st.hello.hello.run()
+options = pd.DataFrame({'Options': ['radio', 'multiselect', 'selectbox']})
 
-with st.echo():
-    options = pd.DataFrame({'Options': ['radio', 'multiselect', 'selectbox']})
-
-    st.radio("Dataframe as input for radio buttons", options)
+st.radio("Dataframe as input for radio buttons", options)
 
 
 """query params"""
-with st.echo():
-    query_params = st.experimental_get_query_params()
-    session_state = SessionState.get(first_query_params=query_params)
-    first_query_params = session_state.first_query_params
-    app_state = st.experimental_get_query_params()
+query_params = st.experimental_get_query_params()
+session_state = SessionState.get(first_query_params=query_params)
+first_query_params = session_state.first_query_params
+app_state = st.experimental_get_query_params()
 
-    radio_list = ['Eat', 'Sleep', 'Both']
-    default_index = eval(first_query_params["radio"][0]) if "radio" in app_state else 0
-    genre = st.radio("What are you doing at home during quarantine?", radio_list, index=default_index)
-    app_state["radio"] = radio_list.index(genre)
+radio_list = ['Eat', 'Sleep', 'Both']
+default_index = eval(first_query_params["radio"][0]) if "radio" in app_state else 0
+genre = st.radio("What are you doing at home during quarantine?", radio_list, index=default_index)
+app_state["radio"] = radio_list.index(genre)
 
-    default_slider = eval(first_query_params["slider"][0]) if "slider" in query_params else 0
-    app_state["slider"] = st.slider("slider", value=default_slider)
-    st.experimental_set_query_params(**app_state)
+default_slider = eval(first_query_params["slider"][0]) if "slider" in query_params else 0
+app_state["slider"] = st.slider("slider", value=default_slider)
+st.experimental_set_query_params(**app_state)
+
+st.hello.hello.run()
