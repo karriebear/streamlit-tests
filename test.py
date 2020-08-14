bde52c0590e73776111fe42c1fd9ed87a632bd4c
karrie <karrie@streamlit.io>
2020-08-14 13:05:19 -0400

    fix session state example

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-import SessionState
+from SessionState import SessionState
+from SessionState import get as get_session_state
 
 st.beta_set_page_config(
     page_title="{0}",
@@ -15,10 +16,10 @@ st.radio("Dataframe as input for radio buttons", options)
 
 st.slider("Slider")
 
-session_state = SessionState.get(user_name='', favorite_color='black')
+session_state = SessionState(user_name='', favorite_color='black')
 session_state.user_name
 
 session_state.user_name = 'Mary'
 
-session_state = get(user_name='', favorite_color='black')
+session_state = get_session_state(user_name='', favorite_color='black')
 session_state.user_name
