f328c2e21e1840e9cf0d459a38e72c974357aa36
karrie <karrie@streamlit.io>
2020-08-14 13:10:51 -0400

    add query params test

--- test.py
+++ test.py
@@ -17,9 +17,27 @@ st.radio("Dataframe as input for radio buttons", options)
 st.slider("Slider")
 
 session_state = SessionState(user_name='', favorite_color='black')
-session_state.user_name
+st.write(session_state.user_name)
 
 session_state.user_name = 'Mary'
-
 session_state = get_session_state(user_name='', favorite_color='black')
-session_state.user_name
+st.write(session_state.user_name)
+
+
+"""query params"""
+query_params = st.experimental_get_query_params()
+app_state = st.experimental_get_query_params()
+
+session_state = SessionState.get(first_query_params=query_params)
+first_query_params = session_state.first_query_params
+
+radio_list = ['Eat', 'Sleep', 'Both']
+
+# The trick here is you can't change the default index based on query params on every run!
+# The *only time* you do that is on the *first* run.
+default_index = eval(first_query_params["radio"][0]) if "radio" in app_state else 0
+
+genre = st.radio("What are you doing at home during quarantine?", radio_list, index=default_index)
+app_state["radio"] = radio_list.index(genre)
+
+st.experimental_set_query_params(**app_state)
