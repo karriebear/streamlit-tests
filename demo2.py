7f7dcfdce248907b14dc54c3974e28db57bdef61
karrie <karrie@streamlit.io>
2020-09-29 13:57:21 -0400

    adrien's demo

---
+++ demo2.py
@@ -0,0 +1,18 @@
+import streamlit as st
+# clear the deprecation warning for the file uploader
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+"# Image Viewer"
+
+IMAGE_EXTENSIONS = ['jpg', 'png', 'jpeg']
+image_data = st.file_uploader('Upload an image', IMAGE_EXTENSIONS)
+
+if not image_data:
+    raise RuntimeError('Please upload a file.')
+
+image = np.array(Image.open(image_data))
+st.image(image, use_column_width=True)
+
+'---'
+if st.checkbox('Show help'):
+    st.help(st.file_uploader)
--- sandbox.py
+++ sandbox.py
@@ -2,6 +2,6 @@ import streamlit as st
 from execbox import execbox
 
 # expander = st.beta_expander("Codepad", expanded=True)
-demo = open('demo.py', 'r')
+demo = open('demo2.py', 'r')
 execbox(demo.read(), autorun=False)
 demo.close()
